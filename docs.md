02044f6f7b573c179d3378040737a06543ae41db
Gyges <shepherd2sovereign@gmail.com>
2026-02-25 07:36:57 -0500

    trouble w tycho compilation

--- docs.md
+++ docs.md
@@ -1,9 +1,3 @@
-- https://docs.propellerheads.xyz/tycho/for-solvers/simulation#installation doesn't work, had to use different toml version to resolve Rust-Analyzer issue "error: failed to load workspaces.". On Cargo version cargo 1.93.1 (083ac5135 2025-12-15).
-```toml
-[dependencies.tycho-simulation]
-git = "https://github.com/propeller-heads/tycho-simulation.git"
-package = "tycho-simulation"
-tag = "0.243.0"
-features = ["evm"]
-```
+- https://docs.propellerheads.xyz/tycho/for-solvers/simulation#installation you can now cargo add which is great.
 - in the fetch token util section, the example is missing a 'compression' parameter which should probably be true: https://docs.propellerheads.xyz/tycho/for-solvers/simulation#step-1-fetch-tokens. see /Users/zwong/.cargo/git/checkouts/tycho-simulation-0ebd7e601264ca0b/13a6d88/src/utils.rs which is also missing the param in docs.
+- ProtocolStreamBuilder in Step 2 should not link to a specific line
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use lifeguard::quote::tycho::measure_depth;
 use tokio::main;
+use tycho_client::feed::component_tracker::ComponentFilter;
 use tycho_core::models::Chain;
-use tycho_simulation::utils::load_all_tokens;
+use tycho_simulation::{
+    evm::engine_db::tycho_db::PreCachedDB, evm::protocol::uniswap_v3::state::UniswapV3State,
+    evm::protocol::vm::state::EVMPoolState, evm::stream::ProtocolStreamBuilder,
+    utils::load_all_tokens,
+};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,6 +23,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Some(1),         // days since last trade (has chain specific defaults)
     )
     .await;
+
+    let tvl_filter = ComponentFilter::with_tvl_range(2000.0, 15000.0);
+    let mut protocol_stream =
+        ProtocolStreamBuilder::new("tycho-beta.propellerheads.xyz", Chain::Ethereum)
+            .exchange::<UniswapV3State>("uniswap_v3", tvl_filter.clone(), None)
+            // add other protocols here
+            .auth_key(Some(tycho_key))
+            .skip_state_decode_failures(true) // skips the pool instead of panicking if it errors on decode
+            .set_tokens(eth_tokens.clone())
+            .await
+            .build()
+            .await
+            .expect("Failed building protocol stream");
 
     Ok(())
 }
